df0995a41875d9d6a1466caa0fe393dd46adcc91
Marcelo M. A <marcelo.munoz@timining.cl>
2016-08-14 02:38:17 -0300

    First commit

---
+++ Cask
@@ -0,0 +1,7 @@
+(source gnu)
+(source melpa)
+
+(package-file "quoted.el")
+
+(development
+ (depends-on "s"))
---
+++ quoted.el
@@ -0,0 +1,126 @@
+;;; quoted.el --- Manage quoted block of text
+
+;; Copyright (C) 2010-2015 Marcelo Muñoz Araya
+
+;; Author: Marcelo Muñoz Araya. <ma.munoz.araya@gmail.com>
+;; URL: https://github.com/marcelino-m/quoted
+;; Keywords: convenience
+;; Version: 0.1.0
+;; Package-Requires: ((s "1.10.0"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+(require 's)
+
+(defun q/quote-line (col-1 col-2 &optional padding)
+  "Quote current line, quote start en column COL-1 and end in column COL-2"
+  (let ((padding (or padding 0)))
+    (save-excursion
+      (move-to-column col-1 t)
+      (insert (concat "\"" (s-repeat padding " ")))
+      (move-to-column (+ col-2 padding) t)
+      (insert (concat (s-repeat padding " ") "\"")) )
+    ))
+
+
+(defun q/quote-region (rbeg rend &optional padding)
+  "Quote region"
+  (interactive "r\nP")
+  (let ((col-left  (q/mincol-no-blanc-in-region rbeg rend))
+        (col-right (q/maxcol-no-blanc-in-region rbeg rend))
+        (last-line (line-number-at-pos rend))
+        (padd      nil))
+    (save-excursion
+      (if (and padding (not (consp padding)))
+          (setq padd (prefix-numeric-value padding)))
+
+      (goto-char rend)
+
+      (when (not (q/empty-before-point-in-line-p rend))
+        (if (> col-right (current-column))
+            (q/quote-line col-left (current-column) padd)
+          (q/quote-line col-left col-right padd)))
+
+      (goto-char rbeg)
+
+      (when (not (q/empty-after-point-in-line-p rend))
+        (if (< col-left (current-column))
+            (q/quote-line (current-column) col-right  padd)
+          (q/quote-line col-left col-right  padd)))
+
+      (while (and (forward-line 1)
+                  (< (line-number-at-pos) last-line))
+        (if (not (q/current-line-empty-p))
+            (q/quote-line col-left col-right  padd))))))
+
+
+(defun q/mincol-no-blanc-in-region (rbeg rend)
+  "Get min column of no blank chars in region, empty lines in
+region not count"
+  (let ((max-line         (line-number-at-pos rend))
+        (min-col          (point-max))
+        (end-of-file-flag nil))
+
+    (save-excursion
+      (goto-char rbeg)
+      (while
+          (progn
+            (back-to-indentation)
+            (if (and (> min-col (current-column))
+                     (not (q/current-line-empty-p)))
+                (setq min-col (current-column)))
+            (and (zerop (forward-line))
+                 (<= (line-number-at-pos) max-line)))))
+    min-col))
+
+(defun q/maxcol-no-blanc-in-region (rbeg rend)
+  "Get min column of no blank chars in region, empty lines in
+region not count"
+  (let ((max-line         (line-number-at-pos rend))
+        (max-col          (point-min)))
+    (save-excursion
+      (goto-char rbeg)
+      (while
+          (progn
+            (end-of-line 1)
+            (skip-syntax-backward " ")
+            (if (< max-col (current-column))
+                (setq max-col (current-column)))
+            (and (zerop (forward-line 1))
+                 (<= (line-number-at-pos) max-line)))))
+    max-col))
+
+(defun q/current-line-empty-p ()
+  (save-excursion
+    (beginning-of-line)
+    (looking-at "[[:space:]]*$")))
+
+(defun q/empty-after-point-in-line-p (arg)
+  (save-excursion
+    (goto-char arg)
+    (looking-at "[[:space:]]*$")))
+
+(defun q/empty-before-point-in-line-p (arg)
+  (save-excursion
+    (goto-char arg)
+    (looking-back "^[[:space:]]*")))
+
+
+;;; quoted.el ends here’
