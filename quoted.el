7b562f47c2d08757c89f0ec358dd6578333a3d31
Marcelo M. A <ma.munoz.araya@gmail.com>
2016-09-04 02:09:00 -0300

    Regroup function

--- quoted.el
+++ quoted.el
@@ -27,124 +27,6 @@
 
 (require 's)
 
-(defun q/quote-current-line-between (col-1 col-2 &optional padding)
-  "Quote current line between COL-1 and COL-2"
-  (let ((padding (or padding 0)))
-    (save-excursion
-      (move-to-column col-2 t)
-      (insert (concat (s-repeat padding " ") "\"")) )
-      (move-to-column col-1 t)
-      (insert (concat "\"" (s-repeat padding " ")))
-    ))
-
-
-
-(defun q/quote-start-position ()
-  "Return the start position of the string at point."
-  (nth 8 (syntax-ppss)))
-
-(defun q/quote-end-position ()
-  "Return the end position of the string at point."
-  (let ((beg (q/quote-start-position)))
-    (save-excursion
-      (goto-char beg)
-      (forward-sexp 1)
-      (skip-syntax-backward "^\"")
-      (point))))
-
-
-(defun q/unquote-current-line ()
-  "Unquote line at pos  if line is quoted"
-  (interactive)
-  (save-excursion
-    (end-of-line)
-    (let ((eol (point))
-          endq
-          startq)
-      (beginning-of-line)
-      (unless (= eol (skip-syntax-forward "^\"" eol))
-        (forward-char)
-        (when (q/line-quoted-at-point-p)
-          (setq endq (q/quote-end-position))
-          (goto-char endq)
-          (delete-char -1)
-          (setq startq (q/quote-start-position))
-          (goto-char startq)
-          (delete-char 1))))))
-
-(defun q/quote-region (rbeg rend &optional padding)
-  "Quote region"
-  (interactive "r\nP")
-  (let ((col-left  (q/mincol-no-blanc-in-region rbeg rend))
-        (col-right (q/maxcol-no-blanc-in-region rbeg rend))
-        (last-line (line-number-at-pos rend))
-        (padd      nil))
-    (when (and col-left col-right)
-      (save-excursion
-        (and
-         padding
-         (not (consp padding))
-         (setq padd (prefix-numeric-value padding)))
-
-        (goto-char rend)
-        (when (not (q/empty-before-point-in-line-p rend))
-          (if (> col-right (current-column))
-              (q/quote-current-line-between col-left (current-column) padd)
-            (q/quote-current-line-between col-left col-right padd)))
-
-        (goto-char rbeg)
-        (unless (= last-line (line-number-at-pos))
-          (when (not (q/empty-after-point-in-line-p rbeg))
-            (if (< col-left (current-column))
-                (q/quote-current-line-between (current-column) col-right  padd)
-              (q/quote-current-line-between col-left col-right  padd)))
-
-          (while (and (forward-line 1)
-                      (< (line-number-at-pos) last-line))
-            (if (not (q/current-line-empty-p))
-                (q/quote-current-line-between col-left col-right  padd))))))))
-
-
-(defun q/mincol-no-blanc-in-region (rbeg rend)
-  "Get min column of no blank chars in region, empty lines in
-region not count, if region contain only blank chars return nil"
-  (let ((max-line         (line-number-at-pos rend))
-        (min-col          (point-max)))
-    (save-excursion
-      (goto-char rbeg)
-      (while
-          (progn
-            (back-to-indentation)
-            (if (and (> min-col (current-column))
-                     (not (q/current-line-empty-p)))
-                (setq min-col (current-column)))
-            (and (zerop (forward-line))
-                 (<= (line-number-at-pos) max-line)))))
-    (if (= min-col (point-max))
-        nil
-      min-col)))
-
-
-(defun q/maxcol-no-blanc-in-region (rbeg rend)
-  "Get min column of no blank chars in region, empty lines in
-region not count, if region contain only blank chars return nil"
-  (let ((max-line         (line-number-at-pos rend))
-        (max-col          (point-min)))
-    (save-excursion
-      (goto-char rbeg)
-      (while
-          (progn
-            (end-of-line 1)
-            (skip-syntax-backward " ")
-            (if (< max-col (current-column))
-                (setq max-col (current-column)))
-            (and (zerop (forward-line 1))
-                 (<= (line-number-at-pos) max-line)))))
-    (if (= max-col (point-min))
-        nil
-      max-col)))
-
-
 
 (defun q/current-line-empty-p ()
   (save-excursion
@@ -192,5 +74,124 @@ region not count, if region contain only blank chars return nil"
              (skip-syntax-backward " ")
              (message "%s" (char-equal quote-char  (char-before)))
              (char-equal quote-char  (char-before))))))))
+
+
+(defun q/quote-current-line-between (col-1 col-2 &optional padding)
+  "Quote current line between COL-1 and COL-2"
+  (let ((padding (or padding 0)))
+    (save-excursion
+      (move-to-column col-2 t)
+      (insert (concat (s-repeat padding " ") "\"")) )
+      (move-to-column col-1 t)
+      (insert (concat "\"" (s-repeat padding " ")))
+    ))
+
+
+(defun q/quote-start-position ()
+  "Return the start position of the string at point."
+  (nth 8 (syntax-ppss)))
+
+
+(defun q/quote-end-position ()
+  "Return the end position of the string at point."
+  (let ((beg (q/quote-start-position)))
+    (save-excursion
+      (goto-char beg)
+      (forward-sexp 1)
+      (skip-syntax-backward "^\"")
+      (point))))
+
+
+(defun q/mincol-no-blanc-in-region (rbeg rend)
+  "Get min column of no blank chars in region, empty lines in
+region not count, if region contain only blank chars return nil"
+  (let ((max-line         (line-number-at-pos rend))
+        (min-col          (point-max)))
+    (save-excursion
+      (goto-char rbeg)
+      (while
+          (progn
+            (back-to-indentation)
+            (if (and (> min-col (current-column))
+                     (not (q/current-line-empty-p)))
+                (setq min-col (current-column)))
+            (and (zerop (forward-line))
+                 (<= (line-number-at-pos) max-line)))))
+    (if (= min-col (point-max))
+        nil
+      min-col)))
+
+
+(defun q/maxcol-no-blanc-in-region (rbeg rend)
+  "Get min column of no blank chars in region, empty lines in
+region not count, if region contain only blank chars return nil"
+  (let ((max-line         (line-number-at-pos rend))
+        (max-col          (point-min)))
+    (save-excursion
+      (goto-char rbeg)
+      (while
+          (progn
+            (end-of-line 1)
+            (skip-syntax-backward " ")
+            (if (< max-col (current-column))
+                (setq max-col (current-column)))
+            (and (zerop (forward-line 1))
+                 (<= (line-number-at-pos) max-line)))))
+    (if (= max-col (point-min))
+        nil
+      max-col)))
+
+
+(defun q/unquote-current-line ()
+  "Unquote line at pos  if line is quoted"
+  (interactive)
+  (save-excursion
+    (end-of-line)
+    (let ((eol (point))
+          endq
+          startq)
+      (beginning-of-line)
+      (unless (= eol (skip-syntax-forward "^\"" eol))
+        (forward-char)
+        (when (q/line-quoted-at-point-p)
+          (setq endq (q/quote-end-position))
+          (goto-char endq)
+          (delete-char -1)
+          (setq startq (q/quote-start-position))
+          (goto-char startq)
+          (delete-char 1))))))
+
+
+(defun q/quote-region (rbeg rend &optional padding)
+  "Quote region"
+  (interactive "r\nP")
+  (let ((col-left  (q/mincol-no-blanc-in-region rbeg rend))
+        (col-right (q/maxcol-no-blanc-in-region rbeg rend))
+        (last-line (line-number-at-pos rend))
+        (padd      nil))
+    (when (and col-left col-right)
+      (save-excursion
+        (and
+         padding
+         (not (consp padding))
+         (setq padd (prefix-numeric-value padding)))
+
+        (goto-char rend)
+        (when (not (q/empty-before-point-in-line-p rend))
+          (if (> col-right (current-column))
+              (q/quote-current-line-between col-left (current-column) padd)
+            (q/quote-current-line-between col-left col-right padd)))
+
+        (goto-char rbeg)
+        (unless (= last-line (line-number-at-pos))
+          (when (not (q/empty-after-point-in-line-p rbeg))
+            (if (< col-left (current-column))
+                (q/quote-current-line-between (current-column) col-right  padd)
+              (q/quote-current-line-between col-left col-right  padd)))
+
+          (while (and (forward-line 1)
+                      (< (line-number-at-pos) last-line))
+            (if (not (q/current-line-empty-p))
+                (q/quote-current-line-between col-left col-right  padd))))))))
 
 ;;; quoted.el ends here
